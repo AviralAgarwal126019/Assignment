8ef1f945ce4ea2ae65251076a98c3d88d530edd3
Aviral <70031430+AviralAgarwal126019@users.noreply.github.com>
2020-08-30 17:40:35 +0530

    Add files via upload

---
+++ testingMath/testingMath.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class testingMath
+{
+    void main ()
+    {
+        int a = 10, b = 20, max, c= 30;
+        System.out.println(Math.pow(2,3));//8
+        System.out.println(Math.pow(3,2));
+        System.out.println(Math.pow(5,2));
+        System.out.println(Math.pow(2,5));
+        System.out.println(Math.min(10,2));
+        max = Math.max(a,b);
+        System.out.println(Math.max(c, max));
+        
+        System.out.println(Math.abs(-67));
+        System.out.println(Math.ceil(3.45));
+        System.out.println(Math.floor(3.45));
+        System.out.println(Math.ceil(-5.45));
+        System.out.println(Math.round(89.50));
+        int z = 10;
+        z = 11;
+        System.out.println(z);
+    }
+}
